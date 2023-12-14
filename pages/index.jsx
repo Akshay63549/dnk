a23a40e1fc82e0f775294e417eb46289208404de
Your Name <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>
2023-12-14 17:46:19 +0530

    create website

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import Categories from '@/components/Categories'
+import Content from '@/components/Content'
+import Cta from '@/components/Cta'
+import Highlight from '@/components/Highlight'
+import Newsletter from '@/components/Newsletter'
+import Products from '@/components/Products'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+    <Content/>
+  <Categories/>
+  <Highlight/>
+    <Products/>
+  <Products/>
+  <Cta/>
+  <Newsletter/>
+    </>
+  )
+}
+
+export default Index
